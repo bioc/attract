3ca552e2427cd5135fc3365b576d8ede8406dfe0
Jessica Mar <jess@jimmy.harvard.edu>
2011-02-25 00:21:13 +0000

    replaced with updated files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/attract@53257 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/classdef.R
+++
@@ -1,6 +0,0 @@
-# attract-specific classes
-
-setClass("AttractorModuleSet", representation(eSet="ExpressionSet", 
-			cellTypeTag="character", incidenceMatrix="matrix", rankedPathways="data.frame"))
-
-setClass("SynExpressionSet", representation(groups="list", profiles="matrix"))
